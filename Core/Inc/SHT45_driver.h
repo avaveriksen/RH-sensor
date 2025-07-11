fff6ebe9de94ff0dd0e1ecdef6f4b091ae1553c4
anders-eriksen <averiksen@proton.me>
2025-07-11 16:31:42 +0200

    Updated SHT45 driver

--- Core/Inc/SHT45_driver.h
+++ Core/Inc/SHT45_driver.h
@@ -14,5 +14,8 @@
 
 uint8_t read_SHT45(I2C_handleTypeDef *hi2c, char variant, uint8_t *buff, float * data_subarray);
 uint8_t read_SHT45_SN(I2C_HandleTypeDef *hi2c, char variant, uint32_t * SN_buff);
+void sensor_power(uint8_t state);
+uint8_t heat_and_read_SHT45(I2C_HandleTypeDef * hi2c, char variant);
+uint8_t CRC_check(uint16_t message, uint8_t checksum);
 
 #endif /* INC_SHT45_DRIVER_H_ */
--- Core/Src/SHT45_driver.c
+++ Core/Src/SHT45_driver.c
@@ -31,9 +31,15 @@ uint8_t read_SHT45(I2C_HandleTypeDef *hi2c, char variant, uint8_t *buff, float *
 	// Convert to real values
 	uint16_t t_ticks = buff[0] << 8 + buff[1];
 	uint8_t checksum_t = buff[2];
+  if (!CRC_check(ticks, checksum_t)) {
+    return 3; // CRC error
+  }
 
 	uint16_t rh_ticks = buff[3] << 8 + buff[4];
 	uint8_t checksum_rh = buff[5];
+  if (!CRC_check(rh_ticks, checksum_rh)){
+    return 3; // CRC error 
+  } 
 
 	float t_degC = -45 + 175 * t_ticks/65535;
 	float rh_pRH = 125 * rh_ticks / 65535 - 6;
@@ -65,7 +71,9 @@ uint8_t read_SHT45_SN(I2C_HandleTypeDef *hi2c, char variant, uint32_t * SN_buff)
 	} else if (variant = 'B') {
 		addr = 0x45;
 		buff_idx = 1;
-	}
+	} else {
+    // error
+  }
 
 	// Request SN data
 	if(HAL_I2C_Master_Transmit(hi2c, addr << 1, 0x89, 2, HAL_MAX_DELAY) != HAL_OK){
@@ -79,4 +87,69 @@ uint8_t read_SHT45_SN(I2C_HandleTypeDef *hi2c, char variant, uint32_t * SN_buff)
 
 	return 0;
 }
+
+void sensor_power(uint8_t state){
+  // Enable or diable power to sensor(s). All sensors are powered by the same source.
+  if(state){
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
+  }
+}
+
+uint8_t reset_SHT45(I2C_HandleTypeDef * hi2c, char variant){
+  // Soft reset of sensor
+  
+  uint8_t addr = 0;
+
+  if(variant == 'A') {
+    addr = 0x44; 
+  } else if (variant == 'B') {
+    addr = 0x45;
+  } else {
+    //error
+  }
+  
+  if(HAL_I2C_Master_Transmit(hi2c, addr << 1, 0x94, 1, HAL_MAX_DELAY) != HAL_OK) {
+    return 2; // I2C error
+  }
+  
+
+}
+
+uint8_t heat_and_read_sht45(I2C_HandleTypeDef * hi2c, char variant) {
+  
+  uint8_t addr = 0;
+  
+  if(variant == 'A') {
+    addr = 0x44; 
+  } else if (variant == 'B') {
+    addr = 0x45;
+  } else {
+    //error
+  }
+  
+  // 0x24 is 110mW for 0.1 second
+
+  if(HAL_I2C_Master_Transmit(hi2c, addr << 1, 0x24, 1, HAL_MAX_DELAY) != HAL_OK) {
+    return 2; // I2C error
+  }
+
+}
+
+uint8_t CRC_check(uint16_t message, uint8_t checksum){
+  // Calculates the Cyclic Redundancy Checksum with polynomial 0x31
+  uint8_t remainder = message % 0x31;
+
+  if (remainder == checksum) {
+    return 1; // success
+  } else {
+    return 0; // Does NOT check out
+  }
+}
+
+
+
+
+
 
