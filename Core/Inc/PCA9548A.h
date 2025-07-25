9cb8219611fa206920a823ca56256342fb1213c1
Anders Eriksen <averiksen@proton.me>
2025-07-25 09:22:02 +0200

    Added i2C Switch driver

---
+++ Core/Inc/PCA9548A.h
@@ -0,0 +1,15 @@
+/*
+ * PCA9548A.h
+ *
+ *  Created on: Jul 24, 2025
+ *      Author: Anders Eriksen
+ */
+
+#ifndef SRC_PCA9548A_H_
+#define SRC_PCA9548A_H_
+
+#define SWITCH_ADDRESS 0x70
+
+#include "main.h"
+
+#endif /* SRC_PCA9548A_H_ */
---
+++ Core/Src/PCA9548A.c
@@ -0,0 +1,30 @@
+/*
+ * PCA9548A.c
+ *
+ *  Created on: Jul 24, 2025
+ *      Author: Anders Eriksen
+ */
+
+#include "PCA9548A.h"
+
+/*
+ * Channel selection: The switch has a control register which is written to or read
+ * by reading/transmitting to the device at its address.
+ */
+
+uint8_t read_switch_control(I2Cdriver * comm){
+
+	if (HAL_I2C_Master_Receive(comm->handle, SWITCH_ADDRESS << 1, comm->i2c_buff, 1, HAL_MAX_DELAY) != HAL_OK){
+		return 2; // I2C error
+	} else {
+		return *comm->i2c_buff;
+	}
+}
+
+uint8_t set_switch_control(I2Cdriver * comm, uint8_t ctrl_byte) {
+
+	if (HAL_I2C_Master_Transmit(comm->handle, SWITCH_ADDRESS, ctrl_byte, 1, HAL_MAX_DELAY) != HAL_OK) {
+		return 2; // I2C error
+	}
+}
+
